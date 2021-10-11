d1df4c5dae043e86a489fa8ca5af0baada287d49
刘r <oncwnuItjWZP5d_W4N8-k9ihUSjQ@git.weixin.qq.com>
2021-10-11 16:22:57 +0800

    10.11

--- README.md
+++ README.md
@@ -4,21 +4,21 @@
 
 linear_layout
 
-![image-20210927190646868](C:\Users\LEGION\AppData\Roaming\Typora\typora-user-images\image-20210927190646868.png)
+![image-20210927190646868](D:\AndroidApp\README.assets\image-20210927190646868.png)
 
 constraint_layout
 
-![image-20210927190812747](C:\Users\LEGION\AppData\Roaming\Typora\typora-user-images\image-20210927190812747.png)
+![image-20210927190812747](D:\AndroidApp\README.assets\image-20210927190812747-16339405109762.png)
 
 table_layout
 
-![image-20210927190845166](C:\Users\LEGION\AppData\Roaming\Typora\typora-user-images\image-20210927190845166.png)
+![image-20210927190845166](D:\AndroidApp\README.assets\image-20210927190845166.png)
 
 实验3
 
-![image-20210928085430449](C:\Users\LEGION\AppData\Roaming\Typora\typora-user-images\image-20210928085430449.png)
+![image-20210928085430449](D:\AndroidApp\README.assets\image-20210928085430449.png)
 
-![image-20210928094836579](C:\Users\LEGION\AppData\Roaming\Typora\typora-user-images\image-20210928094836579.png)
+![image-20210928094836579](D:\AndroidApp\README.assets\image-20210928094836579.png)
 
 
 
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -17,44 +17,10 @@ import java.util.Map;
 import android.view.View;
 
 public class MainActivity extends Activity {
-
-    private ListView listView;
-    private SimpleAdapter simple_adapter;
-    private List<Map<String, Object>> dataList;
-    private String[] name = {"Lion","Tiger","Monkey","Dog","Cat","Elephant"};
-    private int[] images = {R.drawable.lion,R.drawable.tiger,R.drawable.monkey,R.drawable.dog,R.drawable.cat,R.drawable.elephant};
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.simpleadapter);
+        setContentView(R.layout.activity_main);
 
-        //实验3
-        listView = (ListView) findViewById(R.id.listView);
-        dataList = new ArrayList< Map<String, Object>>();
-
-        for(int i = 0;i < 6;i++){
-            Map<String,Object> items = new HashMap<String, Object>(); //创建一个键值对的Map集合，用来存放名字和头像
-            items.put("name", name[i]);      //放入名字， 根据下标获取数组
-            items.put("pic", images[i]);  //放入头像， 根据下标获取数组
-            dataList.add(items);   //把这个存放好数据的Map集合放入到list中，这就完成类数据源的准备工作
-        }
-
-        SimpleAdapter simpleAdapter = new SimpleAdapter(
-                MainActivity.this,/*传入一个上下文作为参数*/
-                        dataList,         /*传入相对应的数据源，这个数据源不仅仅是数据而且还是和界面相耦合的混合体。*/
-                        R.layout.simpleadapterlayout, /*设置具体某个items的布局，需要是新的布局，而不是ListView控件的布局*/
-                        new String[]{"name","pic"}, /*传入上面定义的键值对的键名称,会自动根据传入的键找到对应的值*/
-                        new int[]{R.id.name,R.id.image});/*传入items布局文件中需要指定传入的控件，这里直接上id即可*/
-
-        listView.setAdapter(simpleAdapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                TextView context = (TextView) view.findViewById(R.id.name);
-                Toast.makeText(MainActivity.this, context.getText().toString(), Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
